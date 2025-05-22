7e9d4dd38cc93cf18ce851287ae55d43b95fff89
Moses Karunia <moseskarunia@gmail.com>
2025-05-22 15:14:16 +0700

    display menu

--- order-confirmation/index.js
+++ order-confirmation/index.js
@@ -5,18 +5,30 @@ const totalPrice = parseInt(params.get('totalPrice'));
 
 let content = '';
 
-content += `<div>Total Price = ${totalPrice}</div>`;
+// content += `<div>Total Price = ${totalPrice}</div>`;
 
 for (let i = 0; i < cart.length; i++) {
   const e = cart[i];
 
   if (e[0] > 0) {
-    content += `<div>${menus[i].title} (${menus[i].prices[0].label}): ${e[0]}</div>`;
+    content += `<div class="price-row"><div>${e[0]} ${menus[i].title} (${
+      menus[i].prices[0].label
+    })</div><div>${(
+      e[0] *
+      menus[i].prices[0].price *
+      1000
+    ).toLocaleString()}</div></div>`;
   }
 
   if (e[1] > 0) {
-    content += `<div>${menus[i].title} (${menus[i].prices[1].label}): ${e[1]}</div>`;
+    content += `<div class="price-row"><div>${e[1]} ${menus[i].title} (${
+      menus[i].prices[1].label
+    })</div><div>${(
+      e[1] *
+      menus[i].prices[1].price *
+      1000
+    ).toLocaleString()}</div></div>`;
   }
 }
 
-document.getElementById('outer').innerHTML = content;
+document.getElementById('ordered-menus').innerHTML = content;
